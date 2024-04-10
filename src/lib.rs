220457691b350b386f52797b58a684eeff427411
callqh <528851597@qq.com>
2024-04-10 18:06:36 +0800

    fix: only run in production mode

--- src/lib.rs
+++ src/lib.rs
@@ -52,25 +52,25 @@ impl Plugin for FarmPluginRemoveConsole {
     param: &farmfe_core::plugin::PluginTransformHookParam,
     context: &std::sync::Arc<farmfe_core::context::CompilationContext>,
   ) -> farmfe_core::error::Result<Option<farmfe_core::plugin::PluginTransformHookResult>> {
-    // if matches!(context.config.mode, Mode::Production) {
-    let is_support_module_type = matches!(
-      param.module_type,
-      ModuleType::Js | ModuleType::Jsx | ModuleType::Ts | ModuleType::Tsx
-    );
+    if matches!(context.config.mode, Mode::Production) {
+      let is_support_module_type = matches!(
+        param.module_type,
+        ModuleType::Js | ModuleType::Jsx | ModuleType::Ts | ModuleType::Tsx
+      );
 
-    let filter = PathFilter::new(&self.options.include, &self.options.exclude);
+      let filter = PathFilter::new(&self.options.include, &self.options.exclude);
 
-    // determine if it is a support type
-    // is the path is the user-configured include or exclude
-    if is_support_module_type && filter.execute(&param.resolved_path) {
-      let content = self.regex.replace_all(&param.content, "").into_owned();
+      // determine if it is a support type
+      // is the path is the user-configured include or exclude
+      if is_support_module_type && filter.execute(&param.resolved_path) {
+        let content = self.regex.replace_all(&param.content, "").into_owned();
 
-      return Ok(Some(PluginTransformHookResult {
-        content,
-        ..Default::default()
-      }));
+        return Ok(Some(PluginTransformHookResult {
+          content,
+          ..Default::default()
+        }));
+      }
     }
-
     Ok(None)
   }
 }
