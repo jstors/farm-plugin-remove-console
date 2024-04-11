125506e3c1515c73565b500c5e829c96f44a759f
callqh <528851597@qq.com>
2024-04-11 09:46:28 +0800

    chore: release

---
+++ scripts/release.mjs
@@ -0,0 +1,12 @@
+import {execSync} from 'child_process';
+
+const npmToken = process.env.NODE_AUTH_TOKEN;
+
+if (!npmToken) {
+  console.error('NPM_TOKEN 环境变量未设置！');
+  process.exit(1);
+}
+
+// 为 npm 设置 auth token
+execSync(`npm set //registry.npmjs.org/:_authToken=${npmToken}`);
+execSync(`npx changeset publish`);
