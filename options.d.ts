b55a48d37a2e76ce1711753cb9a806e5db469f87
callqh <528851597@qq.com>
2024-04-30 11:10:40 +0800

    chore: update export

---
+++ options.d.ts
@@ -0,0 +1,10 @@
+export interface IOptions {
+    /**
+     * The path to the root of the project
+     */
+    include: string[],
+    /**
+     * exclude the path from the project
+     */
+    exclude: string[]
+}
--- scripts/index.d.ts
+++ scripts/index.d.ts
@@ -1,2 +1,3 @@
-const binPath: string;
+import type { IOptions } from "../options";
+const binPath: (options: IOptions) =>[string, typeof options];
 export default binPath;
